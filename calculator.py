e5f89ad089cf557aec69dd8977051cd2e631b839
Deniz Duman <dumandenizzzz@gmail.com>
2022-07-24 22:18:58 +1000

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
